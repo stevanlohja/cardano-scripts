8b26a4eb0e7bc6d0cac65185d63f7afb7f27a9e7
Stevan Lohja <stevan.lohja@iohk.io>
2024-02-24 02:01:58 -0500

    feat: start script

---
+++ start_node.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <environment>"
+    echo "Valid environments: preview, preprod, mainnet"
+    exit 1
+fi
+
+ENVIRONMENT="$1"
+NODE_DIR="$HOME/cardano-$ENVIRONMENT"
+PORT=6000
+HOSTADDR=0.0.0.0
+TOPOLOGY="$NODE_DIR/topology.json"
+DB_PATH="$NODE_DIR/db"
+SOCKET_PATH="$NODE_DIR/db/socket"
+CONFIG="$NODE_DIR/config.json"
+
+/usr/local/bin/cardano-node run --topology $TOPOLOGY --database-path $DB_PATH --socket-path $SOCKET_PATH --host-addr $HOSTADDR --port $PORT --config $CONFIG
